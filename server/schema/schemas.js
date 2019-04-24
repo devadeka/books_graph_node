4a7f78dec366ada92d0938c781910add34822140
Devatanu Deka <deva.deka@gmail.com>
2019-04-24 16:35:53 +1000

    mutliple assoc with an author with many books

--- server/schema/schemas.js
+++ server/schema/schemas.js
@@ -4,15 +4,19 @@ const {
   GraphQLObjectType,
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
   GraphQLSchema,
   GraphQLString
 } = graphql;
 
 //dummy data
 var books = [
-  { name: "book1", genre: "genre1", id: "1" },
-  { name: "book2", genre: "genre2", id: "2" },
-  { name: "book3", genre: "genre3", id: "3" }
+  { name: "book1", genre: "genre1", id: "1", authorId: "1" },
+  { name: "book2", genre: "genre2", id: "2", authorId: "2" },
+  { name: "book3", genre: "genre3", id: "3", authorId: "3" },
+  { name: "book4", genre: "genre3", id: "4", authorId: "2" },
+  { name: "book5", genre: "genre4", id: "5", authorId: "3" },
+  { name: "book6", genre: "genre4", id: "6", authorId: "3" }
 ];
 
 var authors = [
@@ -26,7 +30,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 
@@ -35,7 +45,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      }
+    }
   })
 });
 
