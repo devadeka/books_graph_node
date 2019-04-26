15265c28e506a7852ffd1079f46a4ec903c91c5e
Devatanu Deka <deva.deka@gmail.com>
2019-04-26 14:07:37 +1000

    front end added with apollo libs added

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+import ApolloClient from "apollo-boost";
+import ApolloProvider from "react-apollo";
+
+import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <h1>My Books List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <React.Fragment>
+      <ul id='books-list'>
+        <li>Book Name</li>
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default BookList;
